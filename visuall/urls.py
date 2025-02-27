526762262a8c77dd8c8437b2027184c86fd1b268
rajat_saxena <dssiitbhu@gmail.com>
2025-02-27 11:18:16 +0530

    raster_fix

--- visuall/urls.py
+++ visuall/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import get_raster,visual_home,get_raster_lists,get_raster_value
+from .views import get_raster,visual_home,get_raster_lists
 
 urlpatterns = [
     path('',visual_home,name="raster_visual"),
     path('categories/',get_raster,name="raster_categories"),
     path('raster_data/<str:category>/',get_raster_lists,name="raster_details"),
-    path('raster_data/<str:category>/<str:value>/',get_raster_value,name="raster_value")
 ]
